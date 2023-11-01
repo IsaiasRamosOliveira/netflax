db56dc89aa6c7926a074d9b1fd931b799a890101
Jeferson0Silva <jefersonsas12@gmail.com>
2023-11-01 20:20:29 -0300

    feat: modal members

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -1,6 +1,36 @@
-const btn = document.querySelector(".members__btn--adicionar");
+const openbtn = document.querySelector(".item__add");
+const closebtn = document.querySelector(".newMember__close")
 const modal = document.querySelector(".modalMember");
+const submit = document.querySelector(".form__submit");
 
-btn.addEventListener("click", (e) => {
-  modal.style.display = "block";
-})
+const toggleModal = () => {
+  modal.classList.toggle("modalMember__hide");
+};
+
+[openbtn, closebtn].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+
+const member = document.querySelector(".members__groupList");
+const data = [];
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const url = document.querySelector("#url").value;
+  data.push({ url });
+  data.forEach((post) => {  
+    member.innerHTML += `
+    <li class="groupList__item">
+        <a href="/" class="item__listLink">
+          <picture class="listLink__pictures">
+            <img
+              class="pictures__profile"
+              src="${post.url}"
+              alt="profile picture"
+            />
+          </picture>
+        </a>
+      </li>
+  `;
+  });
+});
