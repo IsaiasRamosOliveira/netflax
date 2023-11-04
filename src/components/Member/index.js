18f1d236a5776873d4711daef67b00c0b6174765
Isaías Ramos <isaias.ramos.olive@gmail.com>
2023-11-04 13:57:37 -0300

    feat: modal of members done

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -1,37 +1,61 @@
-const openbtn = document.querySelector(".item__add");
-const closebtn = document.querySelector(".newMember__close")
-const modal = document.querySelector(".modalMember");
-const submit = document.querySelector(".form__submit");
-
-const toggleModal = () => {
-  [modal].forEach((el) => el.classList.toggle("modalMember__hide"));
-};
-
-[openbtn, closebtn, submit].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
-});
-
-const member = document.querySelector(".members__groupList");
+const container = document.querySelector("#container");
 const data = [];
 
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  const url = document.querySelector("#url").value;
-  data.push({ url });
-  data.forEach((post) => {
-    member.innerHTML += `
-    <li class="groupList__item">
-      <a href="/" class="item__listLink">
-        <picture class="listLink__pictures">
-          <img
-            class="pictures__profile"
-            src="${post.url}"
-            alt="profile picture"
-          />
-        </picture>
-      </a>
-    </li>
+function ComponentModal() {
+  return `
+    <div class="modalMember">
+      <div class="modalMember__newMember">
+        <button class="newMember__close" onclick="closeModalMember()">X</button>
+        <div class="newMember__profile">
+          <form class="profile__form">
+            <label class="form__url" for="url">
+              <input class="url__img" type="text" name="url" id="url" placeholder="Enter URL" required/>
+            </label>
+            <button class="form__submitMember" type="submit">Create</button>
+          </form>
+        </div>
+      </div>
+    </div>
   `;
-  });
-});
+}
 
+function ComponentMember(url) {
+  return `
+      <li class="groupList__item">
+        <a href="/" class="item__listLink">
+          <picture class="listLink__pictures">
+            <img
+            class="pictures__profile"
+            src="${url}"
+            alt="profile picture"
+            />
+          </picture>
+        </a>
+      </li>
+    `;
+}
+
+function openModalMember() {
+  container.innerHTML += ComponentModal();
+  submitFormOfMember();
+}
+
+function closeModalMember() {
+  const modal = container.querySelector(".modalMember__newMember");
+  modal.parentElement.remove();
+}
+
+function submitFormOfMember() {
+  const modal = document.querySelector(".modalMember");
+  const form = document.querySelector(".form__submitMember");
+  form.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = modal.querySelector("#url").value;
+    data.push({ url });
+    data.forEach((post) => {
+      const member = document.querySelector(".members__groupList");
+      member.innerHTML += ComponentMember(post.url);
+      closeModalMember();
+    });
+  });
+}
