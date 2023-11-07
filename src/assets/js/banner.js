16a4bc4f4cba116c90783e1f6dde05c293bcbee0
Jeferson0Silva <jefersonsas12@gmail.com>
2023-11-07 17:48:03 -0300

    style: feature banner

--- src/assets/js/banner.js
+++ src/assets/js/banner.js
@@ -1,9 +1,12 @@
 let banner = document.querySelector("#banner");
+let title = document.querySelector(".banner__movie")
 const btns = document.querySelectorAll(".banner__list .banner__mimg");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const img = btn.getAttribute("src");
+    const text = btn.getAttribute("alt");
     banner.style.backgroundImage = `url(${img})`;
+    title.textContent = text;
   });
 });
